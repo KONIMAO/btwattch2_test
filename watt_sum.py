3dd20ad1a57a32d2fc9b08bb5e9df0358ea20f0e
KONIMAO <66402278+KONIMAO@users.noreply.github.com>
2020-12-02 15:15:41 +0900

    add today's date

--- watt_sum.py
+++ watt_sum.py
@@ -3,6 +3,10 @@ from datetime import datetime, date, timedelta
 import json
 decoder = json.JSONDecoder()
 
+today = datetime.today()
+todaysdate = (datetime.strftime(today, '%Y%m%d'))
+print(todaysdate)
+
 lists =[]
 
 with open ('/home/pi/Documents/btwattch2_test/log/20201125.json') as file:
@@ -12,6 +16,6 @@ with open ('/home/pi/Documents/btwattch2_test/log/20201125.json') as file:
     total = sum(lists)
 
     f = open ('watt_per_day.json', '+a')
-    f.write(json.dumps(total) + "\n")
+    f.write('{"Date":' + str(todaysdate) + ', "W":' + json.dumps(total) + '}' + "\n")
     f.close()
     print (total)
