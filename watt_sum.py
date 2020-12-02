f1d374b0a8e9908f3c436d687be84414e5921d9a
KONIMAO <66402278+KONIMAO@users.noreply.github.com>
2020-12-02 14:54:09 +0900

    write the result in another file

--- watt_sum.py
+++ watt_sum.py
@@ -10,4 +10,8 @@ with open ('/home/pi/Documents/btwattch2_test/log/20201125.json') as file:
         data = json.load(line)
         lists.append(data["W"])
     total = sum(lists)
+
+    f = open ('watt_per_day.json', '+a')
+    f.write(json.dumps(total) + "\n")
+    f.close()
     print (total)
