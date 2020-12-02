74eac027028a667b6c8caa697f779aa63cc9e55b
KONIMAO <66402278+KONIMAO@users.noreply.github.com>
2020-12-02 14:39:53 +0900

    now i can print total watt!

--- watt_sum.py
+++ watt_sum.py
@@ -1,13 +1,12 @@
-#from datetime import datetime, date, timedelta
+from datetime import datetime, date, timedelta
 #import time
 import json
-res = []
+lists =[]
 decoder = json.JSONDecoder()
 
 with open ('/home/pi/Documents/btwattch2_test/log/20201125.json') as file:
     for line in file:
         data = json.load(line)
-        lists =[]
         lists.append(data["W"])
-        print(sum(lists))
-        
+    total = sum(lists)
+    print (total)
