a5973274ef756011d7c076f84f115224666baf7f
KONIMAO <66402278+KONIMAO@users.noreply.github.com>
2020-12-09 15:32:45 +0900

    ファイル書き換え

--- copy_delete.py
+++ copy_delete.py
@@ -4,4 +4,4 @@ import os
 import json
 
 os.chdir('/home/pi/Documents/btwattch2_test/per_day')
-shutil.copytree('watt_weekly.json','watt_per_day.json')
+shutil.copytree('./watt_weekly.json','./watt_per_day.json')
