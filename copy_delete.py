37cd9317b1de5f858275f13d8a5f7b6e2ddc85b3
KONIMAO <66402278+KONIMAO@users.noreply.github.com>
2020-12-15 21:11:43 +0900

    bimyou

--- copy_delete.py
+++ copy_delete.py
@@ -1,7 +1,20 @@
-import shutil
-from datetime import datetime, date, timedelta
-import os
+from datetime import datetime
 import json
 
-os.chdir('/home/pi/Documents/btwattch2_test/per_day')
-shutil.copytree('./watt_weekly.json','./watt_per_day.json')
+#BASE_DIR = "/home/pi/Documents/btwattch2_test"
+#PER_DAY_DIR = BASE_DIR + "/per_day"
+
+in_file = "/home/pi/Documents/btwattch2_test/per_day/a.json"
+#out_file = "/home/pi/Documents/btwattch2_test/per_day/a.json"
+
+data = []
+with open(in_file, "r") as f:
+    for l in f:
+        data.append(json.loads(l))
+        
+date = [d.get("Date-maxtime") for d in data]
+maxW = [d.get("maxW") for d in data]
+sumW = [d.get("sumW") for d in data]
+aveW = [d.get("aveW") for d in data]
+log = (date + maxW + sumW +aveW)
+print(log)
